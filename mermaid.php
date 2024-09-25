06470052ae8fd6f0df619571454cf4f3a3f7edbc
Whoopsadaisy <99804696+Whoopsadaisy@users.noreply.github.com>
2024-09-25 22:10:13 +0200

    Update mermaid.php

--- mermaid.php
+++ mermaid.php
@@ -2,7 +2,7 @@
 // Mermaid extension
 
 class YellowMermaid {
-    const VERSION = "0.8.01";
+    const VERSION = "0.9.01";
     public $yellow;         // access to API
     
     // Handle initialisation
@@ -14,7 +14,7 @@ class YellowMermaid {
     public function onParsePageExtra($page, $name) {
         $output = null;
         if ($name=="footer") {
-            $extensionLocation = $this->yellow->system->get("coreServerBase").$this->yellow->system->get("coreExtensionLocation");
+            $assetLocation = $this->yellow->system->get("coreServerBase").$this->yellow->system->get("coreAssetLocation");
             $output="<script>\n";
             $output.="\$mermaid=false;\n";
             $output.="document.querySelectorAll(\"pre.mermaid, pre>code.language-mermaid\").forEach(\$el => {\n";
@@ -25,14 +25,14 @@ class YellowMermaid {
             $output.="})\n";
             $output.="if (\$mermaid) {\n";
             $output.="  js = document.createElement('script');\n";
-            $output.="  js.src =\"{$extensionLocation}mermaid.min.js\";\n";
+		    $output.="  js.src =\"{$assetLocation}mermaid.min.js\";\n";
             $output.="  js.text=\"mermaid.initialize({'theme': 'default', 'securityLevel': 'strict', 'htmlLabels': true, 'fontFamily': ''});\";\n";
             $output.="  document.head.appendChild(js);\n";
             $output.="  js = document.createElement('link');\n";
             $output.="  js.rel=\"stylesheet\";\n";
             $output.="  js.type=\"text/css\";\n";
             $output.="  js.media=\"all\";\n";
-            $output.="  js.href=\"{$extensionLocation}mermaid.css\";\n";
+            $output.="  js.href=\"{$assetLocation}mermaid.css\";\n";
             $output.="  document.head.appendChild(js);\n";
             $output.="}\n";
             $output.="</script>\n";
